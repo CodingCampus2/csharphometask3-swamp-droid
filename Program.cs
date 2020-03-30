ea4ffe373e4b6395295f48fdc302d4547aa571f4
Mykhailo Danilchenko <mykhailo.danilchenko@ubisoft.com>
2020-03-30 10:50:57 +0300

    add working output answer

--- Program.cs
+++ Program.cs
@@ -16,12 +16,46 @@ namespace HomeworkTemplate
                 string UserLatitude = task.UserLatitude;
                 int placesAmount = task.DefibliratorStorages.Length;
 
+                float minDistance = float.MaxValue;
+                string storageName = "";
+                string storageLocation = "";
+
                 for (int i = 0; i < placesAmount; i++)
                 {
                     string defibliratorStorage = task.DefibliratorStorages[i];
-                }   
-                
-                return "My answer";
+
+                    int separatorIndex = defibliratorStorage.IndexOf(';');
+                    string nameStorage = defibliratorStorage.Substring(0, separatorIndex);
+                    defibliratorStorage = defibliratorStorage.Substring(separatorIndex + 1, defibliratorStorage.Length - separatorIndex - 1);
+
+                    separatorIndex = defibliratorStorage.IndexOf(';');
+                    string locationStorage = defibliratorStorage.Substring(0, separatorIndex);
+                    defibliratorStorage = defibliratorStorage.Substring(separatorIndex + 1, defibliratorStorage.Length - separatorIndex - 1);
+
+                    separatorIndex = defibliratorStorage.IndexOf(';');
+                    float longitude = float.Parse(defibliratorStorage.Substring(0, separatorIndex));
+                    defibliratorStorage = defibliratorStorage.Substring(separatorIndex + 1, defibliratorStorage.Length - separatorIndex - 1);
+
+                    float latidute = float.Parse(defibliratorStorage);
+
+                    float userLongitude = (float)(float.Parse(UserLongitude) * Math.PI / 180);
+                    float userLatitude = (float)(float.Parse(UserLatitude) * Math.PI / 180);
+                    longitude = (float)(longitude * Math.PI / 180);
+                    latidute = (float)(latidute * Math.PI / 180);
+
+                    float x = (longitude - userLongitude) * (float)Math.Cos((latidute + userLatitude)/2);
+                    float y = (latidute - userLatitude);
+                    float distance = (float)Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2)) * 6371;
+
+                    if(distance < minDistance)
+                    {
+                        minDistance = distance;
+                        storageName = nameStorage;
+                        storageLocation = locationStorage;
+                    }
+                }
+
+                return $"Name: {storageName}; Address: {storageLocation}";
             };
 
             Task3.CheckSolver(TaskSolver);
